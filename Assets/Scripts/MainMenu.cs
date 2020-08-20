cd6183db2274876f520bd328bcada6bf909a24a3
pizzadelivery01 <50601948+pizzadelivery01@users.noreply.github.com>
2020-08-20 19:20:17 -0400

    Update MainMenu.cs

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,8 +6,21 @@ using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+	
+    public Material trapMat;
+    
+    public Material goalMat;
+    
+    public Toggle colorblindMode;
      public void PlayMaze()
 	 {
+		 if (colorblindMode.isOn){
+            trapMat.color = new Color32(255, 112, 0, 1);
+            goalMat.color = Color.blue;
+        } else{
+            trapMat.color = Color.red;
+            goalMat.color = Color.green;
+        }
 		 SceneManager.LoadScene(1);
 	 }
 	 public void QuitMaze()
